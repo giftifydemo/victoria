bf61b2426d49a2c6ad43ba026a404881a0625853
giftifydemo <112926630+giftifydemo@users.noreply.github.com>
2022-09-06 10:57:05 +0200

    Php second

--- index.php
+++ index.php
@@ -4,3 +4,20 @@ echo "Hello Prout!";
 for ($i=0; $i<100; $i+=10) {
     echo "$i<br>";
     }
+
+
+    <?php
+  $api_url = "https://v2.jokeapi.dev/joke/Any";
+  $joke = json_decode(file_get_contents($api_url));
+?>
+
+
+<?php echo $joke->category;?>
+
+      <?php if($joke->type=='single'){?>
+        <span><?php echo $joke->joke;?></span>
+      <?php } else{ ?>
+        <span><?php echo $joke->setup;?></span>
+        <hr>
+        <span><?php echo $joke->delivery;?></span>
+      <?php } ?>
