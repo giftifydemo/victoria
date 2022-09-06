0e592a056408f03373a8b6ff7f2752bce326948b
giftifydemo <112926630+giftifydemo@users.noreply.github.com>
2022-09-06 10:57:27 +0200

    Third php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<?php
-echo "Hello Prout!";
-
-for ($i=0; $i<100; $i+=10) {
-    echo "$i<br>";
-    }
-
-
     <?php
   $api_url = "https://v2.jokeapi.dev/joke/Any";
   $joke = json_decode(file_get_contents($api_url));
